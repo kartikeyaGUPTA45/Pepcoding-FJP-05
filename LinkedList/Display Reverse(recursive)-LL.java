b5558e653702801502f5cead629acf1c3628ebd2
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2022-05-20 18:56:53 +0530

    Create Display Reverse(recursive)-LL.java

---
+++ LinkedList/Display Reverse(recursive)-LL.java
@@ -0,0 +1,16 @@
+ private void displayReverseHelper(Node node){
+      // write your code here
+      if(node==null){
+          return ;
+      }
+      
+      
+      displayReverseHelper(node.next);
+      System.out.print(node.data+" ");
+      
+    }
+
+    public void displayReverse(){
+      displayReverseHelper(head);
+      System.out.println();
+    }
