a0ed3d9c6f6b37f95430f3945f91d61cd11e55ca
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2022-06-01 13:09:29 +0530

    Create Are Trees Similar in Shape.java

---
+++ Generic Tree/Are Trees Similar in Shape.java
@@ -0,0 +1,21 @@
+
+  public static boolean areSimilar(Node n1, Node n2) {
+    // write your code here
+    if(n1.children.size()!=n2.children.size()){
+        return false;
+        
+    }
+    
+    
+    for(int i=0;i<n1.children.size();i++){
+        Node c1=n1.children.get(i);
+        Node c2=n2.children.get(i);
+        
+        boolean ans=areSimilar(c1,c2);
+        if(ans==false){
+            return false;
+        }
+    }
+    
+    return true;
+  }
