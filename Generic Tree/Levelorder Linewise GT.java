7304ee285c3d92468cdf87d08345b5749d641448
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2022-06-01 12:47:43 +0530

    Create Levelorder Linewise GT.java

---
+++ Generic Tree/Levelorder Linewise GT.java
@@ -0,0 +1,29 @@
+  public static void levelOrderLinewise(Node node){
+    // write your code here
+    
+    //  Create Queue
+    Queue<Node>q=new ArrayDeque<>();
+    //   Add root to the queue
+    q.add(node);
+    
+    
+    //  Run a loop until queue is not empty
+    while(q.size()>0){
+        int sz=q.size();
+        while(sz-->0){
+        
+        // Remove and Print
+        Node nn=q.remove();
+        
+        System.out.print(nn.data+ " ");
+        
+        //  Adding children
+        for(Node child:nn.children){
+            q.add(child);
+        }
+        }
+        System.out.println();
+        
+    }
+    // System.out.println(".");
+  }
