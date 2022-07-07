3f13cc170c07a06cfab516bd77742d8f8f232923
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2022-07-07 12:04:53 +0530

    Create Solve 7n By 8.java

---
+++ Level-02/Bits Manipulation/Solve 7n By 8.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    //write your code here
+    
+    int ans=((n<<3)-n)>>3;
+    
+    System.out.println(ans);
+    
+  }
+
+}
