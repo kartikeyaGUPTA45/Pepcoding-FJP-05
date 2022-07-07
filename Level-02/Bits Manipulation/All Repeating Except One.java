3d9719bfb9f86c1e8f1e1ce4dcb4e6a6811abe97
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2022-07-07 11:55:36 +0530

    Create All Repeating Except One.java

---
+++ Level-02/Bits Manipulation/All Repeating Except One.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for(int i = 0 ; i < n; i++){
+      arr[i] = scn.nextInt();
+    }
+    //write your code here
+    int ans=0;
+    
+    for(int i=0;i<n;i++){
+        ans^=arr[i];
+    }
+    
+    System.out.println(ans);
+    
+  }
+
+}
