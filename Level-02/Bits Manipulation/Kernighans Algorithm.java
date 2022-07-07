8e8c353669b3141d8a2f955781c5994572cef498
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2022-07-07 11:54:54 +0530

    Create Kernighans Algorithm.java

---
+++ Level-02/Bits Manipulation/Kernighans Algorithm.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    
+    //write your code here
+    int count=0;
+    
+    while(n!=0){
+        
+        int rsbm=~(n&(-n));
+        count++;
+        n=n&rsbm;
+    }
+    
+    System.out.println(count);
+  }
+  
+ 
+
+}
