31ac890b9b905fdaa338378bc981a038fa040b29
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2022-07-15 11:01:36 +0530

    Create Combinations-1.java

---
+++ Level-02/Recursion and Backtracking/Combinations-1.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void combinations(int cb, int tb, int ssf, int ts, String asf){
+    // write your code here
+    if(cb>tb){
+        if(ssf==ts){
+            System.out.println(asf);
+        }
+        return ;
+    }
+    
+    
+    // Consider an element
+    combinations(cb+1,tb,ssf+1,ts,asf+"i");
+    
+    //  Not consider an element
+    combinations(cb+1,tb,ssf,ts,asf+"-");
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int nboxes = Integer.parseInt(br.readLine());
+    int ritems = Integer.parseInt(br.readLine());
+    combinations(1, nboxes, 0, ritems, "");
+  }
+
+}
