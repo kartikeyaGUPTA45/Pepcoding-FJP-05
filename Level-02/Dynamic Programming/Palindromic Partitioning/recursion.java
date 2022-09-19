f084efcd3f9056043943de51ac2b9e790ea04d75
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2022-09-19 22:27:03 +0530

    Create recursion.java

---
+++ Level-02/Dynamic Programming/Palindromic Partitioning/recursion.java
@@ -0,0 +1,54 @@
+
+
+//User function Template for Java
+
+class Solution{
+    static boolean isPalindrome(String str,int left,int right){
+        
+        while(left<right){
+            if(str.charAt(left)!=str.charAt(right)){
+                return false;
+            }
+            
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+    static int helper(String str,int i,int j){
+
+        if(i>=j){
+            return 0;
+        }
+
+        if(isPalindrome(str,i,j)==true){
+            return 0;
+        }
+        
+        int ans=Integer.MAX_VALUE;
+        
+        for(int k=i;k<j;k++){
+            int ans1=helper(str,i,k);
+            int ans2=helper(str,k+1,j);
+            int myAns=1;
+            
+            int finalAns=ans1+ans2+myAns;
+            
+            if(finalAns<ans){
+                ans=finalAns;
+            }
+        }
+        
+        return ans;
+    }
+    
+    static int palindromicPartition(String str)
+    {
+        // code here
+        
+        int n=str.length();
+        return helper(str,0,n-1);
+        
+    }
+}
