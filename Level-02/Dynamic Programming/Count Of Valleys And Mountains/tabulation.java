f7bb7aebfc1323b6998ac9aa134b91fd7f90c6d7
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2022-09-19 23:00:10 +0530

    Create tabulation.java

---
+++ Level-02/Dynamic Programming/Count Of Valleys And Mountains/tabulation.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner scn=new Scanner(System.in);
+    int n=scn.nextInt();
+    
+    int dp[]=new int[n+1];
+    
+    dp[0]=1;
+    dp[1]=1;
+    
+    for(int i=2;i<=n;i++){
+        for(int j=0;j<i;j++){
+            dp[i]+=(dp[j]*dp[i-j-1]);
+        }
+    }
+    
+    
+    System.out.println(dp[n]);
+ }
+
+}
